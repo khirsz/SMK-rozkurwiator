b8ffdbe114da0b8f17df4a9a25970eb276cc81eb
Karol Hirsz <karol.hirsz@gmail.com>
2023-12-16 22:29:48 +0100

    Add automatic detection of xpath and some other tweaks to SMKdyzury

--- SMKdyzury.py
+++ SMKdyzury.py
@@ -1,51 +1,47 @@
-# -*- coding: utf-8 -*-
 """
-Created on Mon Dec  7 11:53:23 2020
 SMK Rozkurwiator dyżurów
 @author: Samuel Mazur
 """
 
 import os
+import sys
+from time import sleep
+import selenium.common.exceptions
 from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import pandas as pd
-import xlrd
+import numpy as np
 import tkinter
 
-def arkusz(): 
-    lista = os.listdir('.\\arkusz')
-    duzaDf = pd.DataFrame()
-    for l in range(len(lista)): 
-        xls_file = pd.ExcelFile(os.path.join('.\\arkusz', lista[l]))
-        df = xls_file.parse(0)
-        duzaDf = duzaDf.append(df, ignore_index=True)
-        duzaDf = duzaDf.astype(str)
-    
-    duzaDf = duzaDf[['Liczba godzin', 'Liczba minut', 'Data rozpoczęcia', 'Nazwa komórki organizacyjnej']]
-    
-    for i in range(duzaDf.shape[0]):
-        #konwersja daty
-        duzaDf.iat[i,3] = duzaDf.iat[i,3][0:10]
-    return duzaDf
+def dzialanie(dyzury, driver):
+    WBwait = WebDriverWait(driver, 50, poll_frequency=1)
+    #znajdz przycisk
+    przyciskDodaj = None
+    for przycisk in driver.find_elements(By.XPATH, '//button[text()="Dodaj"]'):
+        if przycisk.is_displayed():
+            przyciskDodaj = przycisk
+            break
+    if przyciskDodaj is None:
+        print("Could not find clickable button! Make sure it is visible in the browser")
+        return
 
-def dzialanie(table, xpath):
-    for i in range(table.shape[0]):
-        #driver.find_element_by_xpath('//button[@title="Dodaj"]').click()
-        #wait.until(EC.element_to_be_clickable((By.XPATH, "//body[1]/div[3]/div[4]/div[1]/table[1]/tbody[1]/tr[1]/td[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/fieldset[1]/table[1]/tbody[1]/tr[2]/td[1]/div[1]/table[1]/tbody[1]/tr[1]/td[1]/div[1]/table[1]/tbody[1]/tr[1]/td[1]/div[1]/div[1]/div[1]/table[1]/tbody[1]/tr[1]/td[1]/div[1]/div[1]/table[1]/tbody[1]/tr[1]/td[1]/div[1]/table[1]/tbody[1]/tr[1]/td[1]/div[1]/div[1]/div[1]/table[1]/tbody[1]/tr[1]/td[1]/div[1]/table[1]/tbody[1]/tr[2]/td[1]/div[1]/table[1]/tbody[1]/tr[5]/td[1]/div[1]/table[1]/tbody[1]/tr[1]/td[1]/div[1]/div[1]/div[1]/table[1]/tbody[1]/tr[1]/td[1]/div[1]/div[1]/table[1]/tbody[1]/tr[1]/td[1]/button[1]"))).click()
-        wait.until(EC.element_to_be_clickable((By.XPATH, xpath))).click()
-    for i in range(table.shape[0]):
+    for dyzur in dyzury:
+        #dodaj
+        WBwait.until(EC.element_to_be_clickable(przyciskDodaj)).click()
         #godziny
-        wait.until(EC.element_to_be_clickable((By.XPATH, "//tbody/tr[" + str(i+1) + "]/td[3]/div[1]/input[1]"))).send_keys(table.iat[i,0])
+        WBwait.until(EC.element_to_be_clickable((By.XPATH, f"//tbody/tr[last()]/td[4]/div[1]/input[1]"))).send_keys(dyzur.godziny)
         #minuty
-        wait.until(EC.element_to_be_clickable((By.XPATH, "//tbody/tr[" + str(i+1) + "]/td[4]/div[1]/input[1]"))).send_keys(table.iat[i,1])
+        WBwait.until(EC.element_to_be_clickable((By.XPATH, f"//tbody/tr[last()]/td[5]/div[1]/input[1]"))).send_keys(dyzur.minuty)
         #data
-        wait.until(EC.element_to_be_clickable((By.XPATH, "//tbody/tr[" + str(i+1) + "]/td[5]/div[1]/input[1]"))).send_keys(table.iat[i,2])
+        WBwait.until(EC.element_to_be_clickable((By.XPATH, f"//tbody/tr[last()]/td[6]/div[1]/input[1]"))).send_keys(dyzur.data)
         #Nazwa komórki organizacyjnej
-        wait.until(EC.element_to_be_clickable((By.XPATH, "//tbody/tr[" + str(i+1) + "]/td[7]/div[1]/input[1]"))).send_keys(table.iat[i,3])
+        WBwait.until(EC.element_to_be_clickable((By.XPATH, f"//tbody/tr[last()]/td[8]/div[1]/input[1]"))).send_keys(dyzur.nazwa)
+
+class Konfiguracja:
+    def __init__(self):
+        self.lokalizacja = None
 
 class Okno(tkinter.Tk):
     def __init__(self, parent):
@@ -57,44 +53,108 @@ class Okno(tkinter.Tk):
         self.grid()
         stepOne = tkinter.LabelFrame(self, text=" Wypełnij zgodnie z instrukcją ")
         stepOne.grid(row=0, columnspan=7, sticky='W',padx=5, pady=5, ipadx=5, ipady=5)
-        
-        self.XpathLbl = tkinter.Label(stepOne,text="Xpath")
-        self.XpathLbl.grid(row=1, column=0, sticky='E', padx=5, pady=2)
-        self.XpathTxt = tkinter.Entry(stepOne)
-        self.XpathTxt.grid(row=1, column=1, columnspan=3, pady=2, sticky='WE')
-        
-        self.xpath = None
-        
-        GuzikWysylania = tkinter.Button(stepOne, text="Wyslij",command=self.wyslij)
-        GuzikWysylania.grid(row=2, column=3, sticky='W', padx=5, pady=2)
-        
-    def wyslij(self):               
-        self.xpath = self.XpathTxt.get()
-        if self.xpath == "":
-            Win2=tkinter.Tk()
-            Win2.withdraw()
-            
+        self.LokalizacjaLbl = tkinter.Label(stepOne,text="Lokalizacja arkuszy")
+        self.LokalizacjaLbl.grid(row=6, column=0, sticky='E', padx=5, pady=2)
+        self.LokalizacjaTxt = tkinter.Entry(stepOne)
+        self.LokalizacjaTxt.grid(row=6, column=1, columnspan=3, pady=2, sticky='WE')
+
+        self.konfiguracja = Konfiguracja()
+
+        self.GuzikWysylania = tkinter.Button(stepOne, text="Wyslij",command=self.wyslij)
+        self.GuzikWysylania.grid(row=7, column=3, sticky='W', padx=5, pady=2)
+
+        self.protocol("WM_DELETE_WINDOW", sys.exit)
+
+    def wyslij(self):
+        self.konfiguracja.lokalizacja = self.LokalizacjaTxt.get()
+        if self.konfiguracja.lokalizacja == "":
+            self.LokalizacjaTxt.config({"background": "Red"})
+            return
+        else:
+            self.LokalizacjaTxt.config({"background": "White"})
+
+        self.LokalizacjaTxt.delete(0, tkinter.END)
+        self.LokalizacjaTxt.insert(0, "")
         self.quit()
 
-#def main():
-driver = webdriver.Chrome(".\\chromedriver.exe")
-options = Options()
-options.add_argument("start-maximized")
-options.add_argument("disable-infobars")
-options.add_argument("--disable-extensions")
-options.add_argument("--log-level=3")
-options.add_experimental_option('excludeSwitches', ['enable-logging'])
-wait = WebDriverWait(driver, 50, poll_frequency=1)
-driver.get("https://smk.ezdrowie.gov.pl/login.jsp?locale=pl")
-tabela = arkusz()
-app = Okno(None)
-app.title("SMK Rozkurwiator Dyżurów 0.1")
-app.mainloop() 
-arg = [app.xpath]
-dzialanie(tabela, *arg)
-input()    
-# if __name__ == "__main__":
-#     main()
+class Dyzur:
+    def __init__(self, data, nazwa, godziny = "24", minuty = ""):
+        self.data = data
+        self.nazwa = nazwa
+        self.godziny = godziny
+        self.minuty = minuty
 
-        
-        
+def zaladujDyzury(lokalizacja):
+    dyzury = []
+
+    try:
+        lokalizacjaUjednolicona = os.path.abspath(lokalizacja)
+        lista = os.listdir(lokalizacjaUjednolicona)
+    except:
+        print("Incorrect path to files!")
+        return
+    for plik in range(len(lista)):
+        try:
+            xls_file = pd.ExcelFile(os.path.join(lokalizacjaUjednolicona, lista[plik]))
+        except ValueError:
+            print(f"Cannot read file {lista[plik]}", file=sys.stderr)
+            continue
+        df = xls_file.parse()
+        #usun nan i inf
+        df = df.replace([np.nan, -np.inf], "")
+
+        godzinyKol = None
+        minutyKol = None
+        dataKol = None
+        nazwaKol = None
+        for kolumna in df.columns:
+            nazwaUjednolicona = str(kolumna).strip().lower()
+            if dataKol is None and "data" in nazwaUjednolicona:
+                dataKol = df[kolumna].dt.strftime("%Y-%m-%d")
+            elif minutyKol is None and "minut" in nazwaUjednolicona:
+                minutyKol = df[kolumna]
+            elif godzinyKol is None and "godzin" in nazwaUjednolicona:
+                godzinyKol = df[kolumna]
+            elif nazwaKol is None and "nazwa" in nazwaUjednolicona:
+                nazwaKol = df[kolumna]
+
+        if dataKol is None or nazwaKol is None:
+            print(f"Incorrect data in file {lista[plik]}", file=sys.stderr)
+            continue
+
+        for i in range(df.shape[0]):
+            if godzinyKol is not None:
+                aktualneGodziny = str(godzinyKol[i])
+            else:
+                aktualneGodziny = "24"
+            if minutyKol is not None:
+                aktualneMinuty = str(minutyKol[i])
+            else:
+                aktualneMinuty = ""
+            #zaladowanie danych
+            dyzury.append(Dyzur(dataKol[i], nazwaKol[i], aktualneGodziny, aktualneMinuty))
+
+    return dyzury
+
+def main():
+    try:
+        driver = webdriver.Chrome()
+    except selenium.common.exceptions.WebDriverException:
+        print("Fallback to Firefox")
+        driver = webdriver.Firefox()
+    driver.maximize_window()
+    driver.get("https://smk.ezdrowie.gov.pl/login.jsp?locale=pl")
+    okno = Okno(None)
+    okno.title("SMK Rozkurwiator Dyżurów 0.2")
+    while True:
+        okno.mainloop()
+        if okno.konfiguracja.lokalizacja is None:
+            sleep(1)
+            continue
+        dyzury = zaladujDyzury(okno.konfiguracja.lokalizacja)
+        if dyzury:
+            dzialanie(dyzury, driver)
+            okno.konfiguracja.lokalizacja = None
+
+if __name__ == "__main__":
+    main()
