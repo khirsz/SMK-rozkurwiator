8a1b76d56a2cc24e71f0b6483315978b35defbd3
Karol Hirsz <karol.hirsz@gmail.com>
2023-12-16 19:54:01 +0100

    Add automatic detection of xpath and some other tweaks

--- smkroz06.py
+++ smkroz06.py
@@ -1,90 +1,81 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Dec  8, 07:19 2020
-SMK ROZKURWIATOR 0.6
-Zmiany: 
-    -dodana obsluga danych osob asystujacych
+SMK ROZKURWIATOR 0.7
 @author: Samuel Mazur
 """
 
 import os
+import sys
+import selenium.common.exceptions
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import pandas as pd
-import xlrd
+import numpy as np
 import tkinter
 
+def dzialanie(pacjenci, konfiguracja, driver):
+    WBwait = WebDriverWait(driver, 50, poll_frequency=1)
+    #znajdz przycisk
+    przyciskDodaj = None
+    for przycisk in driver.find_elements(By.XPATH, '//tbody/tr/td[1]/button[text()="Dodaj"]'):
+        if przycisk.is_displayed():
+            przyciskDodaj = przycisk
+            break
+    if przyciskDodaj is None:
+        print("Could not find clickable button! Make sure it is visible in the browser")
+        return
 
-def arkusz(): 
-    lista = os.listdir('.\\arkusz')
-    duzaDf = pd.DataFrame()
-    for l in range(len(lista)): 
-        xls_file = pd.ExcelFile(os.path.join('.\\arkusz', lista[l]))
-        df = xls_file.parse(0)
-        duzaDf = duzaDf.append(df, ignore_index=True)
-        duzaDf = duzaDf.astype(str)
-    if 'Asysta' in duzaDf.columns:
-        duzaDf = duzaDf[['Nazwisko', 'Imię', 'Usługa', 'Data opisu badania', 'Asysta']]
-        duzaDf.insert(2, 'Plec', '0')
-        duzaDf.insert(5, 'Inicjały', '0')
-    else: 
-        duzaDf = duzaDf[['Nazwisko', 'Imię', 'Usługa', 'Data opisu badania']]
-        duzaDf.insert(2, 'Plec', '0')
-        duzaDf.insert(5, 'Inicjały', '0')
-        duzaDf.insert(6, 'Asysta', "")
-    
-    for i in range(duzaDf.shape[0]):
-        #konwersja daty
-        duzaDf.iat[i,4] = duzaDf.iat[i,4][0:10]
-        #obciecie whitespace i smieci po imieniu
-        head, sep, tail = duzaDf.iat[i,1].partition(' ')
-        duzaDf.iat[i,1] = head
-        #wytworzenie inicjalow
-        duzaDf.iat[i,5] = duzaDf.iat[i,0][0] + '.' + duzaDf.iat[i,1][0] + '.'
-        #identyfikacja plci
-        if duzaDf.iat[i, 1].endswith("A"): 
-            duzaDf.iat[i, 2] = 1
-        #wywalenie nan z asysty
-        if duzaDf.iat[i, 6]=='nan': duzaDf.iat[i,6] = ""
-    return duzaDf
-    
- 
-def dzialanie(table, rok, kod, nazwisko, miejsce, nazwa, xpath):
-    for i in range(table.shape[0]):
-        #driver.find_element_by_xpath('//button[@title="Dodaj"]').click()
-        #wait.until(EC.element_to_be_clickable((By.XPATH, "//body[1]/div[3]/div[4]/div[1]/table[1]/tbody[1]/tr[1]/td[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/fieldset[1]/table[1]/tbody[1]/tr[2]/td[1]/div[1]/table[1]/tbody[1]/tr[1]/td[1]/div[1]/table[1]/tbody[1]/tr[1]/td[1]/div[1]/div[1]/div[1]/table[1]/tbody[1]/tr[1]/td[1]/div[1]/div[1]/table[1]/tbody[1]/tr[1]/td[1]/div[1]/table[1]/tbody[1]/tr[1]/td[1]/div[1]/div[1]/div[1]/table[1]/tbody[1]/tr[1]/td[1]/div[1]/table[1]/tbody[1]/tr[2]/td[1]/div[1]/table[1]/tbody[1]/tr[5]/td[1]/div[1]/table[1]/tbody[1]/tr[1]/td[1]/div[1]/div[1]/div[1]/table[1]/tbody[1]/tr[1]/td[1]/div[1]/div[1]/table[1]/tbody[1]/tr[1]/td[1]/button[1]"))).click()
-        wait.until(EC.element_to_be_clickable((By.XPATH, xpath))).click()
-    for i in range(table.shape[0]):
+    for pacjent in pacjenci:
+        #dodaj
+        WBwait.until(EC.element_to_be_clickable(przyciskDodaj)).click()
         #data
-        wait.until(EC.element_to_be_clickable((By.XPATH, "//tbody/tr[" + str(i+1) + "]/td[2]/div[1]/input[1]"))).send_keys(table.iat[i,4])
+        WBwait.until(EC.element_to_be_clickable((By.XPATH, "//tbody/tr[1]/td[2]/div[1]/input[1]"))).send_keys(pacjent.data)
         #rok szkolenia
-        rokSzkolenia = Select(wait.until(EC.element_to_be_clickable((By.XPATH,"//tbody/tr[" + str(i+1) + "]/td[4]/div[1]/select[1]"))))
-        rokSzkolenia.select_by_value(rok)
-        #kod zabiegu    
-        kodZabiegu = Select(wait.until(EC.element_to_be_clickable((By.XPATH,"//tbody/tr[" + str(i+1) + "]/td[5]/div[1]/select[1]"))))
-        kodZabiegu.select_by_index(str(int(kod)-1))
+        rokSzkolenia = Select(WBwait.until(EC.element_to_be_clickable((By.XPATH,"//tbody/tr[1]/td[4]/div[1]/select[1]"))))
+        rokSzkolenia.select_by_value(konfiguracja.rok)
+        #kod zabiegu
+        kodZabiegu = Select(WBwait.until(EC.element_to_be_clickable((By.XPATH,"//tbody/tr[1]/td[5]/div[1]/select[1]"))))
+        kodZabiegu.select_by_index(str(int(konfiguracja.kod)-1))
         #nazwisko
-        wait.until(EC.element_to_be_clickable((By.XPATH, "//tbody/tr[" + str(i+1) + "]/td[6]/div[1]/input[1]"))).send_keys(nazwisko)
+        WBwait.until(EC.element_to_be_clickable((By.XPATH, "//tbody/tr[1]/td[6]/div[1]/input[1]"))).send_keys(konfiguracja.osoba)
         #miejsce
-        miejscestazu = Select(wait.until(EC.element_to_be_clickable((By.XPATH,"//tbody/tr[" + str(i+1) + "]/td[7]/div[1]/select[1]"))))
-        miejscestazu.select_by_index(miejsce)
+        miejscestazu_element = WBwait.until(EC.element_to_be_clickable((By.XPATH, "//tbody/tr[1]/td[7]/div[1]/select[1]")))
+        miejscestazu = Select(miejscestazu_element)
+        try:
+            miejscestazu.select_by_index(konfiguracja.miejsce)
+        except selenium.common.exceptions.WebDriverException:
+            for j in range(int(konfiguracja.miejsce)):
+                miejscestazu_element.send_keys(Keys.ARROW_DOWN)
         #nazwastazu
-        nazwaStazu = Select(wait.until(EC.element_to_be_clickable((By.XPATH, "//tbody/tr[" + str(i+1) + "]/td[8]/div[1]/select[1]"))))
-        nazwaStazu.select_by_index(nazwa)
+        nazwaStazu_element = WBwait.until(EC.element_to_be_clickable((By.XPATH, "//tbody/tr[1]/td[8]/div[1]/select[1]")))
+        nazwaStazu = Select(nazwaStazu_element)
+        try:
+            nazwaStazu.select_by_index(konfiguracja.nazwa)
+        except selenium.common.exceptions.WebDriverException:
+            for j in range(int(konfiguracja.nazwa)):
+                nazwaStazu_element.send_keys(Keys.ARROW_DOWN)
         #inicjaly
-        wait.until(EC.element_to_be_clickable((By.XPATH, "//tbody/tr[" + str(i+1) + "]/td[9]/div[1]/input[1]"))).send_keys(table.iat[i,5]) 
+        WBwait.until(EC.element_to_be_clickable((By.XPATH, "//tbody/tr[1]/td[9]/div[1]/input[1]"))).send_keys(pacjent.inicjaly)
         #plec
-        plec = Select(wait.until(EC.element_to_be_clickable((By.XPATH,"//tbody/tr[" + str(i+1) + "]/td[10]/div[1]/select[1]"))))
-        if table.iat[i,2]==1: plec.select_by_value('K') 
-        else: plec.select_by_value('M')
+        plec = Select(WBwait.until(EC.element_to_be_clickable((By.XPATH,"//tbody/tr[1]/td[10]/div[1]/select[1]"))))
+        plec.select_by_value(pacjent.plec)
         #asysta
-        wait.until(EC.element_to_be_clickable((By.XPATH, "//tbody/tr[" + str(i+1) + "]/td[11]/div[1]/input[1]"))).send_keys(table.iat[i,6])
+        WBwait.until(EC.element_to_be_clickable((By.XPATH, "//tbody/tr[1]/td[11]/div[1]/input[1]"))).send_keys(pacjent.asysta)
         #nazwaproc
-        wait.until(EC.element_to_be_clickable((By.XPATH, "//tbody/tr[" + str(i+1) + "]/td[12]/div[1]/input[1]"))).send_keys(table.iat[i,3]) 
+        WBwait.until(EC.element_to_be_clickable((By.XPATH, "//tbody/tr[1]/td[12]/div[1]/input[1]"))).send_keys(pacjent.usluga)
+
+class Konfiguracja:
+    def __init__(self):
+        self.rok = None
+        self.kod = None
+        self.osoba = None
+        self.miejsce = None
+        self.nazwa = None
+        self.lokalizacja = None
 
 class Okno(tkinter.Tk):
     def __init__(self, parent):
@@ -100,92 +91,207 @@ class Okno(tkinter.Tk):
         self.RokLbl.grid(row=0, column=0, sticky='E', padx=5, pady=2)
         self.RokTxt = tkinter.Entry(stepOne)
         self.RokTxt.grid(row=0, column=1, columnspan=3, pady=2, sticky='WE')
-        
-        self.KodLbl = tkinter.Label(stepOne,text="Kod zabiegu (1/2)")
+
+        self.KodLbl = tkinter.Label(stepOne,text="Kod zabiegu (które miejsce na liscie)")
         self.KodLbl.grid(row=1, column=0, sticky='E', padx=5, pady=2)
         self.KodTxt = tkinter.Entry(stepOne)
         self.KodTxt.grid(row=1, column=1, columnspan=3, pady=2, sticky='WE')
-        
+
         self.OsobaLbl = tkinter.Label(stepOne,text="Osoba wykonująca")
         self.OsobaLbl.grid(row=2, column=0, sticky='E', padx=5, pady=2)
         self.OsobaTxt = tkinter.Entry(stepOne)
         self.OsobaTxt.grid(row=2, column=1, columnspan=3, pady=2, sticky='WE')
-        
+
         self.MiejsceLbl = tkinter.Label(stepOne,text="Miejsce wykonania (które miejsce na liscie)")
         self.MiejsceLbl.grid(row=3, column=0, sticky='E', padx=5, pady=2)
         self.MiejsceTxt = tkinter.Entry(stepOne)
         self.MiejsceTxt.grid(row=3, column=1, columnspan=3, pady=2, sticky='WE')
-        
+
         self.NazwaLbl = tkinter.Label(stepOne,text="Nazwa stażu (które miejsce na liscie)")
         self.NazwaLbl.grid(row=4, column=0, sticky='E', padx=5, pady=2)
         self.NazwaTxt = tkinter.Entry(stepOne)
         self.NazwaTxt.grid(row=4, column=1, columnspan=3, pady=2, sticky='WE')
-        
-        self.XpathLbl = tkinter.Label(stepOne,text="Xpath")
-        self.XpathLbl.grid(row=5, column=0, sticky='E', padx=5, pady=2)
-        self.XpathTxt = tkinter.Entry(stepOne)
-        self.XpathTxt.grid(row=5, column=1, columnspan=3, pady=2, sticky='WE')
-        
-        self.rok = None
-        self.kod = None
-        self.osoba = None
-        self.miejsce = None
-        self.nazwa = None
-        self.xpath = None
 
-        GuzikWysylania = tkinter.Button(stepOne, text="Wyslij",command=self.wyslij)
-        GuzikWysylania.grid(row=6, column=3, sticky='W', padx=5, pady=2)
+        self.LokalizacjaLbl = tkinter.Label(stepOne,text="Lokalizacja arkuszy")
+        self.LokalizacjaLbl.grid(row=6, column=0, sticky='E', padx=5, pady=2)
+        self.LokalizacjaTxt = tkinter.Entry(stepOne)
+        self.LokalizacjaTxt.grid(row=6, column=1, columnspan=3, pady=2, sticky='WE')
+
+        self.konfiguracja = Konfiguracja()
+
+        self.GuzikWysylania = tkinter.Button(stepOne, text="Wyslij",command=self.wyslij)
+        self.GuzikWysylania.grid(row=7, column=3, sticky='W', padx=5, pady=2)
+
+        self.protocol("WM_DELETE_WINDOW", sys.exit)
 
     def wyslij(self):
-        self.rok = self.RokTxt.get()
-        if self.rok == "":
-            Win2=tkinter.Tk()
-            Win2.withdraw()
+        self.konfiguracja.rok = self.RokTxt.get()
+        if self.konfiguracja.rok == "":
+            self.RokTxt.config({"background": "Red"})
+            return
+        else:
+            self.RokTxt.config({"background": "White"})
 
-        self.kod = self.KodTxt.get()
-        if self.kod == "":
-            Win2=tkinter.Tk()
-            Win2.withdraw()
-            
-        self.osoba = self.OsobaTxt.get()
-        if self.osoba == "":
-            Win2=tkinter.Tk()
-            Win2.withdraw()
+        self.konfiguracja.kod = self.KodTxt.get()
+        if self.konfiguracja.kod == "":
+            self.KodTxt.config({"background": "Red"})
+            return
+        else:
+            self.KodTxt.config({"background": "White"})
 
-        self.miejsce = self.MiejsceTxt.get()
-        if self.miejsce == "":
-            Win2=tkinter.Tk()
-            Win2.withdraw()
-        
-        self.nazwa = self.NazwaTxt.get()
-        if self.nazwa == "":
-            Win2=tkinter.Tk()
-            Win2.withdraw()
-        
-        self.xpath = self.XpathTxt.get()
-        if self.xpath == "":
-            Win2=tkinter.Tk()
-            Win2.withdraw()
-            
+        self.konfiguracja.osoba = self.OsobaTxt.get()
+        if self.konfiguracja.osoba == "":
+            self.OsobaTxt.config({"background": "Red"})
+            return
+        else:
+            self.OsobaTxt.config({"background": "White"})
+
+        self.konfiguracja.miejsce = self.MiejsceTxt.get()
+        if self.konfiguracja.miejsce == "":
+            self.MiejsceTxt.config({"background": "Red"})
+            return
+        else:
+            self.MiejsceTxt.config({"background": "White"})
+
+        self.konfiguracja.nazwa = self.NazwaTxt.get()
+        if self.konfiguracja.nazwa == "":
+            self.NazwaTxt.config({"background": "Red"})
+            return
+        else:
+            self.NazwaTxt.config({"background": "White"})
+
+        self.konfiguracja.lokalizacja = self.LokalizacjaTxt.get()
+        if self.konfiguracja.lokalizacja == "":
+            self.LokalizacjaTxt.config({"background": "Red"})
+            return
+        else:
+            self.LokalizacjaTxt.config({"background": "White"})
+
+        self.LokalizacjaTxt.delete(0, tkinter.END)
+        self.LokalizacjaTxt.insert(0, "")
         self.quit()
-        
 
-#def main():
-driver = webdriver.Chrome(".\\chromedriver.exe")
-options = Options()
-options.add_argument("start-maximized")
-options.add_argument("disable-infobars")
-options.add_argument("--disable-extensions")
-options.add_argument("--log-level=3")
-options.add_experimental_option('excludeSwitches', ['enable-logging'])
-wait = WebDriverWait(driver, 50, poll_frequency=1)
-driver.get("https://smk.ezdrowie.gov.pl/login.jsp?locale=pl")
-tabela = arkusz()
-app = Okno(None)
-app.title("SMK Rozkurwiator 0.6")
-app.mainloop() 
-arg = [app.rok, app.kod, app.osoba, app.miejsce, app.nazwa, app.xpath]
-dzialanie(tabela, *arg)
-input()    
-# if __name__ == "__main__":
-#     main()
+class Pacjent:
+    def wypelnij(self, data, imie, nazwisko, plec = None, usluga = "", asysta = ""):
+        self.data = data
+        self.inicjaly = f"{imie.strip()[0].upper()}.{nazwisko.strip()[0].upper()}."
+        if plec is None:
+            #identyfikacja plci
+            if nazwisko.strip().upper().endswith("A"):
+                self.plec = "K"
+            else:
+                self.plec = "M"
+        else:
+            self.plec = plec.strip()[0].upper()
+        self.asysta = asysta
+        self.usluga = usluga
+
+    def wypelnijInicjalami(self, data, inicjaly, plec, usluga = "", asysta = ""):
+        self.data = data
+        inicjalyUjednolicone = inicjaly.strip().upper().replace(".", "")
+        self.inicjaly = f"{inicjalyUjednolicone[0]}.{inicjalyUjednolicone[1]}."
+        self.plec = plec.strip()[0].upper()
+        self.asysta = asysta
+        self.usluga = usluga
+
+def zaladujPacjentow(lokalizacja):
+    pacjenci = []
+
+    try:
+        lokalizacjaUjednolicona = os.path.abspath(lokalizacja)
+        lista = os.listdir(lokalizacjaUjednolicona)
+    except:
+        print("Incorrect path to files!")
+        return
+    for plik in range(len(lista)):
+        try:
+            xls_file = pd.ExcelFile(os.path.join(lokalizacjaUjednolicona, lista[plik]))
+        except ValueError:
+            print(f"Cannot read file {lista[plik]}", file=sys.stderr)
+            continue
+        df = xls_file.parse()
+        #usun nan i inf
+        df = df.replace([np.nan, -np.inf], "")
+
+        nazwiskoKol = None
+        imieKol = None
+        dataKol = None
+        plecKol = None
+        asystaKol = None
+        inicjalyKol = None
+        uslugaKol = None
+        for kolumna in df.columns:
+            nazwaUjednolicona = str(kolumna).strip().lower()
+            if dataKol is None and "data" in nazwaUjednolicona:
+                dataKol = df[kolumna].dt.strftime("%Y-%m-%d")
+            elif imieKol is None and "imi" in nazwaUjednolicona:
+                imieKol = df[kolumna]
+            elif nazwiskoKol is None and "nazwisko" in nazwaUjednolicona:
+                nazwiskoKol = df[kolumna]
+            elif plecKol is None and ("plec" in nazwaUjednolicona or "płeć" in nazwaUjednolicona):
+                plecKol = df[kolumna]
+            elif asystaKol is None and "asysta" in nazwaUjednolicona:
+                asystaKol = df[kolumna]
+            elif inicjalyKol is None and ("inicjaly" in nazwaUjednolicona or "inicjały" in nazwaUjednolicona):
+                inicjalyKol = df[kolumna]
+            elif uslugaKol is None and ("usluga" in nazwaUjednolicona or "usługa" in nazwaUjednolicona):
+                uslugaKol = df[kolumna]
+
+        if dataKol is None:
+            print(f"Date column not found in file {lista[plik]}", file=sys.stderr)
+            continue
+
+        if imieKol is None or nazwiskoKol is None:
+            if inicjalyKol is None:
+                print(f"Patient data not found in file {lista[plik]}", file=sys.stderr)
+                continue
+            elif plecKol is None:
+                print(f"Patient gender data not found in file {lista[plik]}", file=sys.stderr)
+                continue
+
+        for i in range(df.shape[0]):
+            pacjent = Pacjent()
+            if asystaKol is not None:
+                aktualnaAsysta = asystaKol[i]
+            else:
+                aktualnaAsysta = ""
+            if uslugaKol is not None:
+                aktualnaUsluga = uslugaKol[i]
+            else:
+                aktualnaUsluga = ""
+            #zaladowanie danych
+            if inicjalyKol is None:
+                if plecKol is not None:
+                    aktualnaPlec = plecKol[i]
+                else:
+                    aktualnaPlec = None
+                pacjent.wypelnij(data=dataKol[i], imie=imieKol[i], nazwisko=nazwiskoKol[i],
+                                                plec=aktualnaPlec, usluga=aktualnaUsluga, asysta=aktualnaAsysta)
+            else:
+                pacjent.wypelnijInicjalami(data=dataKol[i], inicjaly=inicjalyKol[i],
+                                                plec=plecKol[i], usluga=aktualnaUsluga, asysta=aktualnaAsysta)
+
+            pacjenci.append(pacjent)
+
+    return pacjenci
+
+def main():
+    try:
+        driver = webdriver.Chrome()
+    except selenium.common.exceptions.WebDriverException:
+        print("Fallback to Firefox")
+        driver = webdriver.Firefox()
+    driver.maximize_window()
+    driver.get("https://smk.ezdrowie.gov.pl/login.jsp?locale=pl")
+    okno = Okno(None)
+    okno.title("SMK Rozkurwiator 0.7")
+    while True:
+        okno.mainloop()
+        if okno.konfiguracja.lokalizacja is None:
+            continue
+        pacjenci = zaladujPacjentow(okno.konfiguracja.lokalizacja)
+        dzialanie(pacjenci, okno.konfiguracja, driver)
+        okno.konfiguracja.lokalizacja = None
+
+if __name__ == "__main__":
+    main()
